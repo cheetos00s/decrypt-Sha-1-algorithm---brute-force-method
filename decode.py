3ad62b3d6486331970007d0c40f851e7470a111c
cheetos00s <96017910+cheetos00s@users.noreply.github.com>
2022-10-13 11:50:18 -0500

    Update decode.py

--- decode.py
+++ decode.py
@@ -16,19 +16,3 @@ for x in list_contraseña.split('\n'):
         et = time.time()
         print('Execution time: ',(et - st)," seconds")
         quit()
-
-
-alp = "abc.....................z"
-
-for x in range(26):
-    for a in range(26):
-        for b in range(26):
-            for c in range(26):
-                for d in range(26):
-                    for e in range(26):
-                        for f in range(26):
-                            for g in range(26):
-                                hashPrueba = hashlib.sha1(str(alp[x]+alp[a]+alp[b]+alp[c]+alp[d]+alp[e]+alp[f]+alp[g])).hexdigest()
-                                print(hashPrueba)
-                                if(hashPrueba == contraseña):
-                                    print("la contraseña es:  ",str(alp[x]+alp[a]+alp[b]+alp[c]+alp[d]+alp[e]+alp[f]+alp[g]))
